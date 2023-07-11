befe378e8f917aa32e1da80d459c688cafaa6b40
Inseok Lee <matt.lee@sendbird.com>
2023-07-11 09:09:14 +0900

    redis-clusters -> rediss-cluster

--- kombu/transport/__init__.py
+++ kombu/transport/__init__.py
@@ -42,7 +42,7 @@ TRANSPORT_ALIASES = {
     'azureservicebus': 'kombu.transport.azureservicebus:Transport',
     'pyro': 'kombu.transport.pyro:Transport',
     'redis-cluster': 'kombu.transport.redis_cluster:Transport',
-    'redis-clusters': 'kombu.transport.redis_cluster:Transport',
+    'rediss-cluster': 'kombu.transport.redis_cluster:Transport',
 }
 
 _transport_cache = {}
